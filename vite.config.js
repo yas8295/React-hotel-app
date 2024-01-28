cb5200e74aa3ae13e991d13ce6c3cd0156d1fc20
mohamed yassin <mohamedyas8295@gmail.com>
2024-01-28 21:34:37 +0200

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import eslint from "vite-plugin-eslint";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), eslint()],
-  base: "/react-hotel-app/",
+  base: "/React-hotel-app/",
 });
